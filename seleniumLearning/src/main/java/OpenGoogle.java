5ae42f564f9c194b6644c7b0584624ac0651c64d
RaviprabhaSanduniJayalal <jayalal12351@usci.ruh.ac.lk>
2025-05-07 00:29:48 +0530

    files  initialized

--- seleniumLearning/src/main/java/OpenGoogle.java
+++ seleniumLearning/src/main/java/OpenGoogle.java
@@ -1,14 +1,14 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OpenGoogle {
 
     public static void main(String[] args) {
-       WebDriverManager.chromedriver().setup();
+
         WebDriver driver = new ChromeDriver();
        driver.get("https://www.google.com");
 //        System.out.println("Google opened successfully");
-//        driver.quit();
+      driver.quit();
     }
 }
